327fc1a82299dc9cff93149b3c5b689abe60cd97
Никита Раненко <nranenko@bk.ru>
2022-03-12 21:17:01 +0300

    Комментарии

--- ViewModels/HourlyQuotesViewModel.cs
+++ ViewModels/HourlyQuotesViewModel.cs
@@ -63,11 +63,11 @@ namespace EcoSphere_Test.ViewModels
 			sw.Start();
 
 			//Словарь для разбиения котировок по часам
-			//Ключ - уникальная пара из даты и часа, List - список котировок за час
+			//Ключ - дата с часом, List - список котировок за этот час
 			Dictionary<DateTime, List<Quote>> aggregatedHourlyQuotes = new();
 			foreach (Quote quote in quotes)
 			{
-				//Если в словаре уже есть такой час в определенную дату, то добавляем котировку к этому часу. Иначе - создаем такой ключ (дату и час)
+				//Если в словаре уже есть такая дата, то добавляем котировку к этой дате. Иначе - создаем такой ключ (дату с часом)
 				DateTime dateKey = new DateTime(quote.Date.Year, quote.Date.Month, quote.Date.Day, quote.Time.Hours, 0, 0);
 				if (aggregatedHourlyQuotes.ContainsKey(dateKey))
 					aggregatedHourlyQuotes[dateKey].Add(quote);
@@ -79,14 +79,23 @@ namespace EcoSphere_Test.ViewModels
 			ICollection<Quote> resultHourlyQuotes = new List<Quote>(aggregatedHourlyQuotes.Count);
 			foreach (var hourlyQuotes in aggregatedHourlyQuotes)
 			{
+				//Символ первой свечи
 				string symbol = hourlyQuotes.Value[0].Symbol;
+				//Описание первой свечи
 				string description = hourlyQuotes.Value[0].Description;
+				//Дата
 				DateTime date = hourlyQuotes.Key.Date;
+				//Время 
 				TimeSpan time = hourlyQuotes.Key.TimeOfDay;
+				//Открытие первой свечи
 				decimal open = hourlyQuotes.Value[0].Open;
+				//Максимум - максимум свечей за этот час
 				decimal high = QuotesUtils.GetMaxQuote(hourlyQuotes.Value).High;
+				//Минимум - минимум свечей за этот час
 				decimal low = QuotesUtils.GetMinQuote(hourlyQuotes.Value).Low;
+				//Закрытие последней свечи
 				decimal close = hourlyQuotes.Value[hourlyQuotes.Value.Count - 1].Close;
+				//Объем - сумма объемов всех свечей за этот час
 				int totalVolume = QuotesUtils.GetTotalVolume(hourlyQuotes.Value);
 
 				resultHourlyQuotes.Add(new Quote(symbol, description, date, time, open, high, low, close, totalVolume));
